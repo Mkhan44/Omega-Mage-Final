e2ed7d2e074d0d776f1bf7a20228c4593b854127
Mohamed R. Khan <khanantisora@hotmail.com>
2016-05-05 00:12:11 -0400

    Stop spell on spiker and slow spell on Bug
    Nice, just finishing touches now.

--- Assets/__Scripts/WaterGroundSpell.cs
+++ Assets/__Scripts/WaterGroundSpell.cs
@@ -4,13 +4,12 @@ using System.Collections;
 //Extends PT_MonoBehaviour
 public class WaterGroundSpell : PT_MonoBehaviour {
 
-	public float duration = 2; //Lifetime of the GameObject.
+	public float duration = 5; //Lifetime of the GameObject.
 	public float durationVariance = 0.5f;
 	// ^ This allows the duration to range from 3.5 to 4.5
 	public float fadeTime = 1f; //Length of time to fade.
 	public float timeStart; //Birth time of this GameObject.
 	public float damagePerSecond = 0;
-
 
 
 	void Start() {
@@ -20,24 +19,23 @@ public class WaterGroundSpell : PT_MonoBehaviour {
 	}
 
 	void Update() {
-	//Determine a number [0 - 1] (between 0 and 1) that stores the
+		//Determine a number [0 - 1] (between 0 and 1) that stores the
 		//% of duration that has passed.
 		float u = (Time.time - timeStart) / duration;
-
 		//At what u value should this start fading.
 		float fadePercent = 1 - (fadeTime / duration);
 		if (u > fadePercent) { 
-		//...then sink into the ground
-			float u2 = (u-fadePercent) /(1-fadePercent);
+			//...then sink into the ground
+			float u2 = (u - fadePercent) / (1 - fadePercent);
 			// ^u2 is a number (0,1) for just the fadeTime
 			Vector3 loc = pos;
-			loc.z = u2*2; //move lower over time
+			loc.z = u2 * 2; //move lower over time
 			pos = loc;
 		}
 
 		if (u > 1) {
-			Destroy(gameObject); //...Destroy it.
-			}
+			Destroy (gameObject); //...Destroy it.
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -48,18 +46,17 @@ public class WaterGroundSpell : PT_MonoBehaviour {
 		}
 		Utils.tr ("Water hit", go.name);
 
-	}
-
-	void OnTriggerStay(Collider other) {
 		EnemyBug recipient = other.GetComponent<EnemyBug> ();
 		if (recipient != null) {
-			recipient.speed = 0.0f;
+			recipient.speed = 0.1f;
 		}
 		EnemySpiker recipient2 = other.GetComponent<EnemySpiker> ();
 		if (recipient2 != null) {
-			recipient2.speed = 0.0f;
+			recipient2.speed =  0.0f;
+			recipient2.touchDamage = 0;
 		}
 	}
+
 
 	void OnTriggerExit(Collider other) {
 		//When enemy exits this field or the instance dissapears, movement speed goes back to normal.
@@ -68,9 +65,6 @@ public class WaterGroundSpell : PT_MonoBehaviour {
 			if (recipient != null) {
 			recipient.speed = 0.5f;
 		}
-		EnemySpiker recipient2 = other.GetComponent<EnemySpiker> ();
-		if (recipient2 != null) {
-			recipient2.speed = 5f;
-		}
+	
 	  }
 }
